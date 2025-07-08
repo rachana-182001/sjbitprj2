f710eae397e3e48f7664a83b2f1e94a6b9b2a4eb
rachana-182001 <rachana2001ak@gmail.com>
2025-07-08 15:46:10 +0530

    second commit updated

--- src/dsa/mca/collection/DemoDSA.java
+++ src/dsa/mca/collection/DemoDSA.java
@@ -9,6 +9,39 @@ import java.util.TreeSet;
 public class DemoDSA {
 
 	public static void main(String[] args) {
+		
+		LinkedList<Integer> l=new LinkedList<Integer>();
+		l.add(100);
+		l.addLast(200);
+		System.out.println(l);
+		
+		
+		
+		TreeSet<Integer> h=new TreeSet<Integer>();
+		h.add(200);
+		h.add(300);
+		h.add(400);
+		h.add(300);
+		System.out.println(h);
+		
+		
+		
+		ArrayList<Integer>n=new ArrayList<>();
+		n.add(10);
+		n.add(20);
+		n.add(3);
+		n.addFirst(78);
+		n.add(2,400);
+		n.sort(null);
+		System.out.println(n);
+		System.out.println(n.get(3));
+
+
+
+
+
+
+
 		HashMap<Integer,String> d=new HashMap<Integer,String>();
 		d.put(100, "desktop");
 		d.put(101, "ram");
